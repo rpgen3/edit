ac362b03099bc386333fc40251ce9b5aae7ba472
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-06-11 18:20:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (async()=>{
+    await import("https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js");
     await Promise.all([
         "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js",
         "https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ext-language_tools.js",
         "https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.10.0/beautify.js"
     ].map(v=>import(v)));
