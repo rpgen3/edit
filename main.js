c9165f6be8178774a9bdac5b41b08650645c5401
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-06-12 16:53:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,7 @@
     const getScript = url => new Promise((resolve, reject)=>$.getScript(url).done(resolve).fail(reject));
     await getScript("https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js");
     await getScript("https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ext-language_tools.js");
-    const h = $('body');
-    $('<div>').appendTo(h).prop('id','editor').css({
+    $('<div>').appendTo('body').prop('id','editor').css({
         height: '90vh'
     });
     const editor = ace.edit('editor');
@@ -18,22 +17,55 @@
         enableLiveAutocompletion: true
     });
     editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/javascript");
+    editor.session.setMode("ace/mode/javascript");
     window.e = editor;
+    const sign = '/*\0\0\0*/',
+          reg = / ?\/\*\0\0\0\*\//;
     $('textarea').get(0).addEventListener('keyup', e => {
-        const sign = '/*\0\0\0*/',
-              reg = / ?\/\*\0\0\0\*\//;
         if(e.ctrlKey || !/^[a-zA-Z0-9]{1}$/.test(e.key)) return;
-        const s = editor.getSession();
-        s.insert(editor.getCursorPosition(), sign);
-        const result = js_beautify(s.getValue(),{
+        editor.session.insert(editor.getCursorPosition(), sign);
+        const result = js_beautify(editor.session.getValue(),{
             max_preserve_newlines: 2
         });
-        s.setValue(result.replace(reg,''));
-        const ar = result.slice(0, result.indexOf(sign)).split('\n');
-        editor.moveCursorToPosition({
-            row: ar.length - 1,
-            column: ar.pop().length
-        });
+        const str = result.replace(reg,''),
+              ar = result.slice(0, result.indexOf(sign)).split('\n'),
+              pos = {
+                  row: ar.length - 1,
+                  column: ar.pop().length
+              };
+        input({str, pos});
+        history.add({str, pos});
     });
+    $('textarea').get(0).addEventListener('keyup', e => {
+        if(!e.ctrlKey) return;
+        switch(e.key){
+            case 'z': return undo();
+            case 'Z': return redo();
+        }
+    });
+    const input = ({str, pos}) => {
+        if(!pos) return;
+        editor.session.setValue(str);
+        editor.moveCursorToPosition(pos);
+    };
+    const history = new class {
+        constructor() {
+            this.now = null;
+            this.add(null);
+        }
+        add(data) {
+            this.now = {
+                data, prev: this.now
+            };
+            if(this.now.prev) this.now.prev.next = this.now;
+        }
+        undo() {
+            return this.now.prev ? (this.now = this.now.prev).data : null;
+        }
+        redo() {
+            return this.now.next ? (this.now = this.now.next).data : null;
+        }
+    }
+    const undo = () => input(history.undo()),
+          redo = () => input(history.redo());
 })();
