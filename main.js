7a6803f362b66873dc02bbbe823acf104e5a3a82
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-06-12 11:15:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 (async()=>{
     await Promise.all([
         "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js",
-        "https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ext-language_tools.js",
         "https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.10.0/beautify.js"
     ].map(v=>import(v)));
+    const getScript = url => new Promise((resolve, reject)=>$.getScript(url).done(resolve).fail(reject));
+    await getScript("https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ace.js");
+    await getScript("https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0/ext-language_tools.js");
     const h = $('body');
     $('<div>').appendTo(h).prop('id','editor').css({
-        height: 600
+        height: '90vh'
     });
     var editor = ace.edit("editor");
     editor.$blockScrolling = Infinity;
